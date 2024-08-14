a71ca22ed6b00da4cb7fb09496736760834cd0f3
soft_code <emmanuel.erhabor98@gmail.com>
2024-08-14 03:44:47 +0100

    learning backend

--- routes/cache/redis/_middleware.dart
+++ routes/cache/redis/_middleware.dart
@@ -12,10 +12,20 @@ Handler middleware(Handler handler) {
   return (context) async{
     Response response;
 
-    ///Now we are going to connect to redis i.e the one we have installed in our docker
-    ///The connect method connects to the redis server
     try{
+      ///Now we are going to connect to redis i.e the one we have installed in our docker
+      ///The connect method connects to the redis server
       final command = await conn.connect('localhost', 6379);
+      ///This is to authenticate our connection
+      ///I didn't set password when i was installing redis via command line so i
+      ///left this as an example of how it would look like:
+      //await command.send_object(['AUTH', 'default', 'password']);
+      try{
+        response = await handler.use(provider<Command>((_) => command)).call(context);
+      }catch(e){
+        response = Response.json(body: {'success': false, 'message': e.toString()});
+      }
+
     }catch(e){
       response = Response.json(body: {'success': false, 'message': e.toString()});
     }
--- routes/cache/redis/index.dart
+++ routes/cache/redis/index.dart
@@ -1,6 +1,49 @@
-import 'package:dart_frog/dart_frog.dart';
+import 'dart:io';
 
-Response onRequest(RequestContext context) {
-  final value = context.read<String>();
-  return Response(body: value);
+import 'package:dart_frog/dart_frog.dart';
+import 'package:redis/redis.dart';
+
+
+//1 - logged in, 0 - logged out
+///so if we receive in our temporary cache in redis that our status is 0 that means
+///the user should be logged out and vice-versa
+Future<Response> onRequest(RequestContext context) {
+  return switch(context.request.method){
+    HttpMethod.get => _getLoginStatus(context),
+    HttpMethod.post => _setLoginStatus(context),
+    _=> Future.value(Response(statusCode: HttpStatus.methodNotAllowed))
+  };
 }
+
+///I want to use redis cache to handle or store our login status
+Future<Response> _getLoginStatus (RequestContext context) async{
+  final value = await context.read<Command>().send_object(['GET', 'loggedin'])
+      .then((value) => value);
+
+  if(value == null){
+    const status = 0;
+    await context.read<Command>().send_object(['SET', 'loggedin', status]);
+    return Response(statusCode: HttpStatus.noContent);
+  }else{
+    return Response.json(body: value.toString() );
+  }
+}
+
+Future<Response> _setLoginStatus (RequestContext context) async{
+  final body = await context.request.json() as Map<String, dynamic>;
+  final status = body['loggedin'] as int;
+  var success = false;
+
+  try{
+    ///we set the value of our key to the value that user has passed which in
+    /// this case is "status"
+    await context.read<Command>().send_object(['SET', 'loggedin', status]);
+    success = true;
+  }catch(e){
+    success = false;
+  }
+
+  return Response.json(body: {'success': success});
+}
+
+
