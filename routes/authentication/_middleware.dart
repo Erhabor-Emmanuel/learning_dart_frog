0453baa8c96d006a1e5968cabaa143221e8938cd
soft_code <emmanuel.erhabor98@gmail.com>
2024-08-21 15:00:05 +0100

    learning backend

--- routes/authentication/_middleware.dart
+++ routes/authentication/_middleware.dart
@@ -1,6 +1,20 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:dart_frog_auth/dart_frog_auth.dart';
+import 'package:learnfrog_backend/repository/user/user_repository.dart';
 
 Handler middleware(Handler handler) {
-  // TODO: implement middleware
-  return handler;
+  return handler.use(provider<UserRepository>((_) => UserRepository()))
+
+  .use(basicAuthentication<User>(
+    authenticator: (context, username, password){
+    /// This variable gets the injected UserRepository
+    /// and can now have access to its functions
+    final repository = context.read<UserRepository>();
+    return repository.userFromCredentials(username, password);
+  },
+      /// This authentcation request only applies if the httpMethod is not a
+      /// POST request
+    applies: (RequestContext context) async =>
+      context.request.method != HttpMethod.post
+  ));
 }
